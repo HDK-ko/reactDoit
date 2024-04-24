37f3f604fa3b287a101aead9fa0a3453d4e200a9
HOME\Hwang.Dongkuk <hwang_dongkuk@radcns.com>
2024-04-24 17:01:50 +0900

    20240424

---
+++ ch02/ch02_2/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singhleQuote: true,
+  trailingComma: "none",
+  arrowParens: "avoid",
+  semi: false,
+  printWidth: 90,
+};
---
+++ ch02/ch02_3/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ClassComponent from "./ClassComponent";
+import ArrowComponent from "./ArrowComponent";
+
+export default function App() {
+  return (
+    <ul>
+      <ClassComponent href="http://www.google.com" text="go to Google" />
+      <ArrowComponent href="https://twitter.com" text="go to Twitter" />
+    </ul>
+  );
+}
