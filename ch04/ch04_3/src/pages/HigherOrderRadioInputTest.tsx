3bf641cd5dbe3625a1bef6d5544febb8761b891c
DESKTOP-NLG2FOM\c <lakeibt15@gmail.com>
2024-05-08 23:04:43 +0900

    [home] 20240508

--- ch04/ch04_3/src/pages/HigherOrderRadioInputTest.tsx
+++ ch04/ch04_3/src/pages/HigherOrderRadioInputTest.tsx
@@ -1,8 +1,41 @@
-export default function HigherOrderRadioInputTest() {
+import {useCallback, useMemo, useState} from 'react'
+import * as D from '../data'
+import {Subtitle, Title} from '../components'
+
+export default function RadioInputTest() {
+  const jobTitles = useMemo(() => D.makeArray(4).map(D.randomJobTitle), [])
+  const [selectedIndex, setSelectedIndex] = useState<number>(0)
+  const onChange = useCallback(
+    (index: number) => () => setSelectedIndex(notUsed => index),
+    []
+  )
+  const radioInputs = useMemo(
+    () =>
+      jobTitles.map((value, index) => (
+        <label key={index} className="flex justify-start cursor-pointer label">
+          <input
+            type="radio"
+            name="jobs"
+            className="mr-4 radio radio-primary"
+            checked={index === selectedIndex}
+            onChange={onChange(index)}
+          />
+          <span className="label-text">{value}</span>
+        </label>
+      )),
+    [jobTitles, selectedIndex, onChange]
+  )
+
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">CopyMe</h2>
-      <div className="mt-4"></div>
+      <Title>RadioInputTest</Title>
+      <div className="flex flex-col justify-center mt-4">
+        <Subtitle>What is your job?</Subtitle>
+        <Subtitle className="mt-4">Selected Job: {jobTitles[selectedIndex]}</Subtitle>
+        <div className="flex justify-center p-4 mt-4">
+          <div className="flex flex-col mt-4">{radioInputs}</div>
+        </div>
+      </div>
     </section>
   )
 }
--- ch04/ch04_3/src/pages/ShowHideModal.tsx
+++ ch04/ch04_3/src/pages/ShowHideModal.tsx
@@ -1,8 +1,37 @@
+import {useCallback} from 'react'
+import {useToggle} from '../hooks'
+import {Subtitle, Title} from '../components'
+import {Button, Modal, ModalAction, ModalContent} from '../theme/daisyui'
+import * as D from '../data'
+
 export default function ShowHideModal() {
+  const [open, toggleOpen] = useToggle(false)
+  const onAccept = useCallback(() => {
+    toggleOpen()
+  }, [toggleOpen])
+
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">CopyMe</h2>
-      <div className="mt-4"></div>
+      <Title>ShowHideModal</Title>
+      <div className="flex justify-center p-4">
+        <Button className="btn-primary" onClick={toggleOpen}>
+          open modal
+        </Button>
+      </div>
+      <Modal open={open}>
+        <ModalContent
+          closeIconClassName="btn-primary btn-outline"
+          onCloseIconClicked={toggleOpen}>
+          <Subtitle>Modal </Subtitle>
+          <p>{D.randomParagraphs()}</p>
+          <ModalAction>
+            <Button className="btn-primary" onClick={onAccept}>
+              ACCEPT
+            </Button>
+            <Button onClick={toggleOpen}>CLOSE</Button>
+          </ModalAction>
+        </ModalContent>
+      </Modal>
     </section>
   )
 }
