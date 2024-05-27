9220db914c80f44ea535e4d8d896dbbbd55332b9
DESKTOP-NLG2FOM\c <lakeibt15@gmail.com>
2024-05-28 00:07:17 +0900

    [home] 20240528

--- ch04/ch04_5/src/pages/ForwardRefTest.tsx
+++ ch04/ch04_5/src/pages/ForwardRefTest.tsx
@@ -1,8 +1,28 @@
+import {useCallback, useEffect, useRef} from 'react'
+import {Title} from '../components'
+import {Button, Input} from '../theme/daisyui'
+
 export default function ForwardRefTest() {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const getValue = useCallback(
+    () => alert(`input value : ${inputRef.current?.value}`),
+    []
+  )
+
+  useEffect(() => inputRef.current?.focus(), [])
+
   return (
     <section className="mt-4">
-      <h2 className="font-bold text-5xl text-center">CopyMe</h2>
-      <div className="mt-4"></div>
+      <Title>InputValueTest</Title>
+      <div className="flex justify-center mt-4">
+        <div className="flex flex-col w-1/3 p-2">
+          <Input ref={inputRef} className="input-primary" />
+          <Button onClick={getValue} className="mt-4 btn-primary">
+            GET VALUE
+          </Button>
+        </div>
+      </div>
     </section>
   )
 }
